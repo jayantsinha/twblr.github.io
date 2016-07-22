e8d11b2d77756db3103d728a5041be08957c484c
Jayant Sinha <jayant.sinha@hotmail.com>
2016-07-22 15:33:46 +0530

    code fixed

--- control_structures/if_structure.go
+++ control_structures/if_structure.go
@@ -1,5 +1,19 @@
 package control_structures
 
-func fizzBuzz(i int32) string {
-	return ""
+func fizzBuzz(i int32) (r string) {
+	//r string
+	if i%2 == 0 {
+		r = "2"
+	}
+	if i%3 == 0 {
+		if i%5 == 0 {
+			r = "FizzBuzz"
+		} else {
+			r = "Fizz"
+		}
+	} 
+	if i%5 == 0 && i%3 != 0 {
+		r = "Buzz"
+	}
+	return
 }
