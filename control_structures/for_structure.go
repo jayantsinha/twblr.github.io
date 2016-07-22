a96ef16af834d6239ec72b5b8f55418a42524e3c
Jayant Sinha <jayant.sinha@hotmail.com>
2016-07-22 16:10:06 +0530

    for_structure code complete

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,5 +1,17 @@
 package control_structures
 
-func collatzChainLength(n int) int {
-	return 0
+func collatzChainLength(n int) (itr int) {
+	
+	for itr =0; ;itr++ {
+		if n%2==0 {
+			n = n/2
+		} else {
+			n = (3*n)+1
+		}
+		if n==1 {
+			break
+		}
+	}
+
+	return itr+1
 }
