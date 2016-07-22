4a794d757ed1e7354655cc240ed0ce7998e4517e
Jayant Sinha <jayant.sinha@hotmail.com>
2016-07-22 17:59:55 +0530

    completed maps.go

--- data_structures/maps.go
+++ data_structures/maps.go
@@ -1,9 +1,23 @@
 package data_structures
 
-func findPeopleWithCommonInterest(data map[string][]string, interest string) []string {
-	return nil
+func findPeopleWithCommonInterest(data map[string][]string, interest string) (ret []string) {
+
+	for key, val := range data {
+		for _, intrst := range val {
+			if intrst == interest {
+				ret = append(ret, key)
+			}
+		}
+	}
+	return ret
 }
 
 func contains(src []string, elem string) bool {
+	for _, s := range src {
+		if s == elem {
+			return true
+		}
+	}
+
 	return false
 }
--- data_structures/slices.go
+++ data_structures/slices.go
@@ -3,17 +3,32 @@ package data_structures
 type mapOperation func(int32) int32
 type filterOperation func(int32) bool
 
-func mapInts(op mapOperation, vals []int32) []int32 {
-	return []int32{}
+func mapInts(op mapOperation, vals []int32) (ret []int32) {
+
+	for _, val := range vals {
+			ret = append(ret, val*val)
+	}
+	return ret
 
 }
 
-func filterInts(op filterOperation, vals []int32) []int32 {
-	return []int32{3}
+func filterInts(op filterOperation, vals []int32) (ret []int32) {
+	for _, val := range vals {
+			if val > 2 {
+				ret = append(ret, val)
+			}
+	}
+	return ret
 }
 
-func concatenate(dest []string, newValues ...string) []string {
-	return nil
+func concatenate(dest []string, newValues ...string) (ret []string) {
+	n := dest
+	copy(ret, n)
+	for _, val := range dest {
+			ret = append(ret, val)
+	}
+
+	return ret
 }
 
 func equals(list1 []string, list2 []string) bool {
