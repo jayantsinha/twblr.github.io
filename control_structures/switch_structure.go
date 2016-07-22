a232ea31bb421cfef778c2e92c7e526afc2fe8ea
Jayant Sinha <jayant.sinha@hotmail.com>
2016-07-22 15:48:29 +0530

    switch_structure completed

--- control_structures/switch_structure.go
+++ control_structures/switch_structure.go
@@ -5,6 +5,17 @@ const subtract string = "-"
 const multiply string = "*"
 const divide = "/"
 
-func calculate(op string, arg1, arg2 float32) float32 {
-	return 0.0
+func calculate(op string, arg1, arg2 float32) (a float32) {
+	switch op {
+	case add:
+		a = arg1+arg2
+	case subtract:
+		a = arg1-arg2
+	case multiply:
+		a = arg1*arg2
+	case divide:
+		a = arg1/arg2
+
+	}
+	return
 }
